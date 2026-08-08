635743bd2a07be5ae230c424b900d3a5cd5681d3
Jeronimo Moreno Herrera <jeronimo.moreno-h@mail.escuelaing.edu.co>
2026-08-08 16:21:18 -0500

    chore:discord webhook test

--- dosw-lab-02/src/main/java/edu/eci/dosw/App.java
+++ dosw-lab-02/src/main/java/edu/eci/dosw/App.java
@@ -2,10 +2,10 @@ package edu.eci.dosw;
 
 /**
  * Hello world!
+ * test
  */
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
-    
 }
